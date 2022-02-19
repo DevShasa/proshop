56d7a02569de6dff3d874e3e5ee43019eb56968d
DevShasa <shasa.thuo@gmail.com>
2022-02-19 13:27:56 +0300

    Product detail page works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
-// import ProductScreen from './screens/ProductScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
+
+
   return (
     <Router>
       <Header />
         <main className="py-3">
           <Container>
             <Route exact path="/" component={HomeScreen}/>
-            {/* <Route path="/product/:id" component={ProductScreen}/> */}
+            <Route path="/product/:id" component={ProductScreen}/>
           </Container>
         </main>
       <Footer />
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -10,7 +10,7 @@ const HomeScreen = () =>{
     useEffect(()=>{
 
         async function fetchProduct(){
-            const {data} = await axios.get('http://127.0.0.1:8000/api/products/')
+            const {data} = await axios.get('/api/products/')
             setProducts(data)
         }
 
