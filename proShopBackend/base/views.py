01fbe8890963dbb56d99e234059f472ae8aa5ee8
DevShasa <shasa.thuo@gmail.com>
2022-02-19 12:24:35 +0300

    Added django gitignore

--- proShopBackend/base/views.py
+++ proShopBackend/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
+# from django.http import JsonResponse
 
 # Create your views here.
 @api_view(['GET'])
