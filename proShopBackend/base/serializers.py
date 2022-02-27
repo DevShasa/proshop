dbf63c6a00b5dcf6068b143c5b8ebe99a57f0fa7
DevShasa <shasa.thuo@gmail.com>
2022-02-27 08:58:04 +0300

    sucessfully implemented simple jwt

--- proShopBackend/base/serializers.py
+++ proShopBackend/base/serializers.py
@@ -1,6 +1,19 @@
+# This is where we pick what items from the database to serialize 
 from rest_framework import serializers
 from django.contrib.auth.models import User 
 from .models import Product
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod 
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        # token['message'] = 'Hello World'
+        token['is_superuser'] = user.is_superuser
+        token['is_staff'] = user.is_staff
+        return token
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
--- proShopBackend/base/views.py
+++ proShopBackend/base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# from .products import products
-# from django.http import JsonResponse
 from .models import Product
-from .serializers import ProductSerializer 
-# Create your views here.
+from .serializers import ProductSerializer, MyTokenObtainPairSerializer 
+# CUSTOMISING THE USER ACCESS TOKEN
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
