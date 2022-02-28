e083e946ad8cfd0ba59e3bbd5af96d160a06dc7a
DevShasa <shasa.thuo@gmail.com>
2022-02-28 20:47:52 +0300

    Changes to the simpleJWT token data

--- proShopBackend/base/views.py
+++ proShopBackend/base/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from django.contrib.auth.models import User 
 from .models import Product
-from .serializers import ProductSerializer, MyTokenObtainPairSerializer, UserSerializer 
+from .serializers import ProductSerializer, MyTokenObtainPairSerializer, UserSerializer, UserSerializerWithToken
+from django.contrib.auth.hashers import make_password 
 # CUSTOMISING THE USER ACCESS TOKEN
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -12,26 +13,30 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload/',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
-    ]
-    return Response(routes)
-
-@api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    # Create the user and return the associated user object 
+    print('DATA: ', request.data)
+    print('Request: ', request)
+
+    user = User.objects.create(
+        first_name = data['name'],
+        username = data['email'],
+        email = data['email'],
+        password = make_password(data['password'])
+    )
+    # Create a noice token for the newly created user 
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     '''
     Accept a bearer jwt token in the GET request header
@@ -41,14 +46,16 @@ def getUserProfile(request):
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
 
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id = pk)
-    # for i in products:
-    #     if i['_id'] == pk:
-    #         product = i
-    #         break
     serializer = ProductSerializer(product,many=False)
-
     return Response(serializer.data)
+
