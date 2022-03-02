0fa7757bc65009f7da0737ec8a6f0bf905efa096
DevShasa <shasa.thuo@gmail.com>
2022-03-02 03:22:21 +0300

    cleaned up urls

--- proShopBackend/base/views/user_views.py
+++ proShopBackend/base/views/user_views.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer, MyTokenObtainPairSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import  MyTokenObtainPairSerializer, UserSerializer, UserSerializerWithToken
 from django.contrib.auth.hashers import make_password 
 from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -12,11 +11,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
 
 @api_view(['POST'])
 def registerUser(request):
@@ -56,9 +50,5 @@ def getUsers(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id = pk)
-    serializer = ProductSerializer(product,many=False)
-    return Response(serializer.data)
+
 
