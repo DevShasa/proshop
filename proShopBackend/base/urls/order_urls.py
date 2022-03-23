d67691b6d6ec967bcbb20a5655cbf40660b36df2
Shasa <shasa.thuo@gmail.com>
2022-03-23 21:08:17 +0300

    getmyorders endpoint

--- proShopBackend/base/urls/order_urls.py
+++ proShopBackend/base/urls/order_urls.py
@@ -3,6 +3,8 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name="order-add"),
+    path('myorders/', views.getMyOrders, name="myorders"),
     path('<str:id>/', views.getOrderById, name="get-product"),
-    path('<str:pk>/pay/', views.updateOrderToPaid, name="pay")
+    path('<str:pk>/pay/', views.updateOrderToPaid, name="pay"),
+
 ]
