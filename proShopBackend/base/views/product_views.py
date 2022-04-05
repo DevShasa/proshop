4e885319ee55781651cf3709f8964d25cafde76e
Shasa <shasa.thuo@gmail.com>
2022-04-05 18:15:55 +0300

    Upload file functionality

--- proShopBackend/base/views/product_views.py
+++ proShopBackend/base/views/product_views.py
@@ -73,7 +73,7 @@ def uploadImage(request):
     data = request.data
     product_id = data['product_id']
     product = Product.objects.get(_id = product_id)
-    # product.image = request.FILES.get('image')
+    # product.image = request.FILES.get('newProductImage')
     print('<<REQUEST>> ', request.FILES)
     product.image = data['newProductImage']
     product.save()
